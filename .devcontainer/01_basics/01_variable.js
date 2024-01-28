a3f97941b3601b45e67e74caa3b0649280c590e4
chimadage Amol <131466503+AmolchimadagE@users.noreply.github.com>
2024-01-28 10:05:21 +0000

    conversion of datatype

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,26 @@
+const accountId = 12345
+let accountEmail = "Aomol@google.com"
+var accountpassword = "12345"
+accouncity ="bangalore"
+
+let accountstate;
+
+
+// accountId = 2 //note allow
+
+accountEmail = "hc@hac.com123"
+accountpassword = "2121234"
+accountcity = "maharashtra"
+
+
+
+console.log(accountEmail);
+
+/* prefer not to use var
+because  of issue in block scope and functional scope   
+*/
+
+console.table([accountId,accountEmail,accountpassword,accountcity,accountstate]);
+
+
+
---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,25 @@
+let score = 33
+let Name ="Amol"
+let arti ='amol'
+console.log(typeof score);
+console.log(typeof (arti));
+
+//console.log (typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInnumber);
+//console.log(valueInNumber);
+ 
+let isLoggedIn = "Amol"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=> true; 0=>false
+// "" => false 
+// "nitesh" => true
+ let someNumber = 33
+ let stringNumber = string(someNumber)
+ console.log( typeof stringNumber);
+
