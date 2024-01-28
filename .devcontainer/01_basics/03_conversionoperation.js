492383b0aa252a98322e16415f8ceda1658a65f9
chimadage Amol <131466503+AmolchimadagE@users.noreply.github.com>
2024-01-28 10:38:20 +0000

    comparision.js

--- .devcontainer/01_basics/03_conversionoperation.js
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -4,22 +4,57 @@ let arti ='amol'
 console.log(typeof score);
 console.log(typeof (arti));
 
-//console.log (typeof score);
-//console.log(typeof(score));
+console.log (typeof score);
+console.log(typeof(score));
 
-//let valueInNumber = Number(score)
-//console.log(typeof valueInnumber);
-//console.log(valueInNumber);
+let valueInNumber = Number(score)
+console.log(typeof valueInnumber);
+console.log(valueInNumber);
  
-let isLoggedIn = "Amol"
+//let isLoggedIn = "Amol"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanIsLoggedIn);
 
 //1=> true; 0=>false
 // "" => false 
 // "nitesh" => true
- let someNumber = 33
- let stringNumber = string(someNumber)
- console.log( typeof stringNumber);
+
+
+
+
+
+ //**************************************** operations**** */
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(6/2);
+// console.log( (2+3) * 5%3);
+
+
+// let str1 = "Hello"
+// let str2 = "mummy"
+// let str3 = str1 + str2
+// console.log(str3)
+
+let num1,num2,num3
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
 
---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2" >1);
+console.log(null>1);
+console.log(null>=0);
+
+//tc 39 ecma
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+//===
+console.log("2" === 2);
+
+
+
+
+
