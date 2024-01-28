1d427e4617781145c5e03175996cb339a5213e76
chimadage Amol <131466503+AmolchimadagE@users.noreply.github.com>
2024-01-28 11:40:35 +0000

    datatype-summery

---
+++ .devcontainer/01_basics/datatype-summery.js
@@ -0,0 +1,36 @@
+// primitive
+
+// 7 type: string , number, boolean, null, undefined, symbol, BigInt,
+
+// const score = 100
+// const scorevalue = 100.3
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = symbol('123')
+// const anotherId = symbol('123')
+// console.log(id=== anotherId);
+
+
+
+
+//refrence (non-primitive datatype)
+
+//Array, object functions
+
+const heros =["A.chimadage","sagar prasad","vikash singh"];
+let myobj = {name:"hitesh",age: 22,}
+
+const myFunction = function(){
+    console.log("Hello Mom");
+
+}
+console.log ( typeof heros );
+
+
+//type of opreator function is function null=>is object 
+
+
+
+
