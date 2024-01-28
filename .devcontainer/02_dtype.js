ae80aee10770f672d3ee97319f8128981c6eee46
chimadage Amol <131466503+AmolchimadagE@users.noreply.github.com>
2024-01-28 09:35:34 +0000

    datype

---
+++ .devcontainer/02_dtype.js
@@ -0,0 +1,22 @@
+
+let name = "Amol chimadage"
+let age = 24
+let isloggedin = false
+let state ;
+
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean =>true/flase 
+//null => standalone value
+//undefined =>
+//symbol =>unique 
+
+//object 
+
+console.log(typeof "name");
+console.log(typeof "age");
+console.log(typeof "null");
+
+
